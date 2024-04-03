c7b618a44d68f86cfedab749edb26bbe321b1013
HyeonJunSong <music9801@naver.com>
2024-04-03 17:59:20 +0900

    refactor: directory fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useState } from "react";
-import MainPage from "../../public/pages/main_page";
-import CartPage from "../../public/pages/cart_page";
-import InfoPage from "../../public/pages/info_page";
+import MainPage from "./components/pages/main_page";
+import CartPage from "./components/pages/cart_page";
+import InfoPage from "./components/pages/info_page";
 
 export default function Home() {
   const [pageIdx, setPageIdx] = useState(0);
