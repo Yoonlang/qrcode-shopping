d243d60661ae5bc81b5d1a2209b7901d6c73262a
HyeonJunSong <music9801@naver.com>
2024-04-12 16:15:40 +0900

    refactor: seperate exports

--- src/constants/colors.tsx
+++ src/constants/colors.tsx
@@ -1,7 +1,5 @@
-const WHITE = "#ffffff";
-const BLACK = "#000000";
+export const WHITE = "#ffffff";
+export const BLACK = "#000000";
 
-const PRIMARY_DARK = "#323232";
-const PRIMARY = "#42FF00";
-
-export { WHITE, BLACK, PRIMARY_DARK, PRIMARY };
+export const PRIMARY_DARK = "#323232";
+export const PRIMARY = "#42FF00";
