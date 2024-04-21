5d384ea26d84ad4a20713cb204ea05b53cbfa538
yoonlang <cdt9473@gmail.com>
2024-04-21 13:33:07 +0900

    refactor: 페이지 레이어 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,9 @@
 "use client";
-import CartPage from "@/pages/cart_page";
-import InfoPage from "@/pages/info_page";
-import MainPage from "@/pages/main_page";
-import GlobalStyle from "@/styles/global";
-import { useCallback, useState } from "react";
+
+import MainPage from "@/components/pages/MainPage";
 
 const Home = () => {
-  const [pageIdx, setPageIdx] = useState(2);
-  const [scannedItems, setScannedItems] = useState({});
-
-  const toNextPage = useCallback(() => {
-    setPageIdx((pageIdx + 1) % 3);
-  }, [pageIdx]);
-
-  return (
-    <>
-      <GlobalStyle />
-      {pageIdx === 0 ? (
-        <MainPage toNextPage={toNextPage} setScannedItems={setScannedItems} />
-      ) : pageIdx === 1 ? (
-        <CartPage toNextPage={toNextPage} scannedItems={scannedItems} />
-      ) : (
-        <InfoPage toNextPage={toNextPage} />
-      )}
-    </>
-  );
+  return <MainPage />;
 };
 
 export default Home;
