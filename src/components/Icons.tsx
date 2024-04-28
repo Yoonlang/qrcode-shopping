4d9cec30cf71999357f42e2feaa4c25a14c2795a
nullyng <nullyng@gmail.com>
2024-04-28 15:32:08 +0900

    feat: svg 파일 추가

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -11,6 +11,11 @@ const Icons: {
   ),
   person: <Image src={"/svg/person.svg"} width={25} height={25} alt={"사람"} />,
   check: <Image src={"/svg/check.svg"} width={25} height={25} alt={"체크"} />,
+  list: <Image src={"/svg/list.svg"} width={12} height={12} alt={"목록"} />,
+  delete: <Image src={"/svg/delete.svg"} width={14} height={14} alt={"삭제"} />,
+  open: <Image src={"/svg/open.svg"} width={7} height={11} alt={"열기"} />,
+  close: <Image src={"/svg/close.svg"} width={7} height={11} alt={"닫기"} />,
+  select: <Image src={"/svg/select.svg"} width={11} height={12} alt={"선택"} />,
 };
 
 export default Icons;
