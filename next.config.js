904c390a5366cfd72ee897ebfbca9c71a35b8a3c
yoonlang <cdt9473@gmail.com>
2024-04-30 23:09:40 +0900

    배포 환경에 맞게 서버 URL 처리

--- next.config.js
+++ next.config.js
@@ -2,4 +2,10 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
+  env: {
+    SERVER_URL:
+      process.env.NODE_ENV === "production"
+        ? `https://server.jojoywmaeil.com`
+        : `https://localhost:5001`,
+  },
 };
