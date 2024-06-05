010f65d79527d9720b4700a1cfcf29fc088d53ab
yoonlang <cdt9473@gmail.com>
2024-06-05 17:32:42 +0900

    test: dialog wechat test

--- src/components/pages/QrScannerPage.tsx
+++ src/components/pages/QrScannerPage.tsx
@@ -1,5 +1,10 @@
 import QrCode from "@/components/QrCode";
 import { MessageSnackBar } from "@/components/SnackBar";
+import { Button, Dialog } from "@mui/material";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
@@ -28,7 +33,12 @@ const QrScannerPage = ({
   setSnackBarStatus: Dispatch<SetStateAction<string>>;
   snackBarStatusMessage: object;
 }) => {
+  const [openDialog, setOpenDialog] = useState(true);
   const { t } = useTranslation();
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
 
   useEffect(() => {
     if (Object.keys(scannedItems).length > 0) {
@@ -39,13 +49,34 @@ const QrScannerPage = ({
 
   return (
     <StyledContainer>
-      <MessageSnackBar
-        key={`${Object.keys(scannedItems).length} ${snackBarStatus}`}
-        isOpen={snackBarOpen}
-        setIsOpen={setSnackBarOpen}
-        message={snackBarStatus}
-      />
-      <QrCode setScannedItems={setScannedItems} fetchedItems={fetchedItems} />
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {!openDialog && (
+        <>
+          <MessageSnackBar
+            key={`${Object.keys(scannedItems).length} ${snackBarStatus}`}
+            isOpen={snackBarOpen}
+            setIsOpen={setSnackBarOpen}
+            message={snackBarStatus}
+          />
+          <QrCode
+            setScannedItems={setScannedItems}
+            fetchedItems={fetchedItems}
+          />
+        </>
+      )}
     </StyledContainer>
   );
 };
