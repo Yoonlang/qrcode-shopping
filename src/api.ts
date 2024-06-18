3bb31da824252ed8fe469087dac56840fa9ae4e4
yoonlang <cdt9473@gmail.com>
2024-06-18 18:26:48 +0900

    refactor: api 중첩 코드 따로 빼내기

--- src/api.ts
+++ src/api.ts
@@ -1,37 +1,44 @@
 import { SERVER_URL } from "@/components/const";
 
+const http = {
+  get: (path, onSuccess, onFail) =>
+    fetch(`${SERVER_URL}${path}`, {
+      method: "GET",
+    })
+      .then((res) =>
+        res.json().then((data) => {
+          if (!res.ok) {
+            throw new Error(data.error);
+          }
+          return data;
+        })
+      )
+      .then(onSuccess)
+      .catch(onFail),
+  post: (path, headers, body, onSuccess, onFail) =>
+    fetch(`${SERVER_URL}${path}`, {
+      method: "POST",
+      headers: headers ?? {
+        "Content-Type": "application/json",
+      },
+      body,
+    })
+      .then((res) =>
+        res.json().then((data) => {
+          if (!res.ok) {
+            throw new Error(data.error);
+          }
+          return data;
+        })
+      )
+      .then(onSuccess)
+      .catch(onFail),
+};
+
 export const getProductList = (onSuccess, onFail) => {
-  return fetch(`${SERVER_URL}/products`, {
-    method: "get",
-  })
-    .then((res) =>
-      res.json().then((data) => {
-        if (!res.ok) {
-          throw new Error(data.error);
-        }
-        return data;
-      })
-    )
-    .then(onSuccess)
-    .catch(onFail);
+  return http.get(`/products`, onSuccess, onFail);
 };
 
 export const submitOrdererInfo = (body, onSuccess, onFail) => {
-  return fetch(`${SERVER_URL}/users-info`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body,
-  })
-    .then((res) =>
-      res.json().then((data) => {
-        if (!res.ok) {
-          throw new Error(data.error);
-        }
-        return data;
-      })
-    )
-    .then(onSuccess)
-    .catch(onFail);
+  return http.post(`/users-info`, null, body, onSuccess, onFail);
 };
