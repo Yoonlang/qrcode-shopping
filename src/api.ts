96142ec08bc5060aa7225098961defb5647cfdb8
yoonlang <cdt9473@gmail.com>
2024-06-18 18:14:20 +0900

    feat: submitOrdererInfo 구현

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import { SERVER_URL } from "@/components/const";
 
-export const getProductList = (success, fail) => {
+export const getProductList = (onSuccess, onFail) => {
   return fetch(`${SERVER_URL}/products`, {
     method: "get",
   })
@@ -12,6 +12,26 @@ export const getProductList = (success, fail) => {
         return data;
       })
     )
-    .then(success)
-    .catch(fail);
+    .then(onSuccess)
+    .catch(onFail);
+};
+
+export const submitOrdererInfo = (body, onSuccess, onFail) => {
+  return fetch(`${SERVER_URL}/users-info`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
+  })
+    .then((res) =>
+      res.json().then((data) => {
+        if (!res.ok) {
+          throw new Error(data.error);
+        }
+        return data;
+      })
+    )
+    .then(onSuccess)
+    .catch(onFail);
 };
