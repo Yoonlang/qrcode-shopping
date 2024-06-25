2e5a53d8ae478fb72ec00a2ac42002514cd69c80
yoonlang <cdt9473@gmail.com>
2024-06-25 17:23:09 +0900

    feat: fetchedItemListSelector 구현

--- src/recoil/atoms/fetchedItemListState.ts
+++ src/recoil/atoms/fetchedItemListState.ts
@@ -1,8 +1,23 @@
-import { atom } from "recoil";
+import { selector } from "recoil";
 
 import { ProductType } from "@/components/ToBuyList/ToBuyItem/const";
+import { getProductList } from "@/api";
 
-export const fetchedItemListState = atom<ProductType[]>({
-  key: "fetchedItemListState",
-  default: [],
+export const fetchedItemListSelector = selector<ProductType[]>({
+  key: "fetchedItemListSelector",
+  get: async () => {
+    try {
+      const getProductListPromise = () =>
+        new Promise<ProductType[]>((resolve, reject) => {
+          return getProductList(
+            (data) => resolve(data),
+            (e) => reject(e)
+          );
+        });
+      return await getProductListPromise();
+    } catch (e) {
+      console.log(e);
+      return [];
+    }
+  },
 });
