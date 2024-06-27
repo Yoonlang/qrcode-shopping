e753e57f683e2e1fab8cc1369058764f52b14f3a
yoonlang <cdt9473@gmail.com>
2024-06-27 16:09:19 +0900

    test: QrCode test 구현

---
+++ src/components/QrScanner/QrCode.test.tsx
@@ -0,0 +1,57 @@
+import { RecoilRoot, useRecoilValueLoadable } from "recoil";
+import { act, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import QrCode from "@/components/QrScanner/QrCode";
+
+jest.mock("recoil", () => ({
+  ...jest.requireActual("recoil"),
+  useRecoilValueLoadable: jest.fn(),
+}));
+
+describe("QrCode loading 확인", () => {
+  it("fetchedItemList가 로딩 중일 때, Fragment 반환", () => {
+    // Given
+    (useRecoilValueLoadable as jest.Mock).mockReturnValue({
+      state: "loading",
+    });
+
+    // When
+    const { container } = render(
+      <RecoilRoot>
+        <QrCode />
+      </RecoilRoot>
+    );
+
+    // Then
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it("fetchedItemList 불러오기 성공 시, camera DOMElement 반환", async () => {
+    // Given
+    (useRecoilValueLoadable as jest.Mock).mockReturnValue({
+      state: "hasValue",
+      contents: [
+        {
+          productId: "test",
+        },
+      ],
+    });
+
+    // When
+    let container;
+    await act(async () => {
+      const renderResult = render(
+        <RecoilRoot>
+          <QrCode />
+        </RecoilRoot>
+      );
+
+      container = renderResult.container;
+    });
+
+    // Then
+    const videoElement = container.querySelector("video");
+    expect(videoElement).toBeInTheDocument();
+  });
+});
