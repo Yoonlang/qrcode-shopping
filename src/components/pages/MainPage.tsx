097b02f47eb406a93fef99416ae9ef9f9098bb3d
nullyng <nullyng@gmail.com>
2024-06-28 17:14:41 +0900

    refactor: MainPage에서 SplashScreen 로직 분리

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,47 +1,25 @@
 import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
-import { useFormik } from "formik";
-import { Suspense, useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { useSetRecoilState } from "recoil";
+import { Suspense } from "react";
 
 import { BottomAppBar, TitleAppBar } from "@/components/AppBar";
-import { snackBarStatusMessage } from "@/components/const";
 import QrScannerPage from "@/components/pages/QrScannerPage";
 import ToBuyListPage from "@/components/pages/ToBuyListPage";
 import UserInfoSubmissionPage from "@/components/pages/UserInfoSubmissionPage";
 import SplashScreen from "@/components/SplashScreen";
 import usePageRouter from "@/hooks/usePageRouter";
-import { messageSnackBarState } from "@/recoil/atoms/messageSnackBarState";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault("Asia/Seoul");
 
 const MainPage = () => {
-  const { t } = useTranslation();
   const { isPageName } = usePageRouter();
-  const [isSplashScreenOpen, setIsSplashScreenOpen] = useState(false);
-  const setMessageSnackBarState = useSetRecoilState(messageSnackBarState);
-
-  useEffect(() => {
-    if (!sessionStorage.getItem("splash")) {
-      setIsSplashScreenOpen(true);
-      setTimeout(() => {
-        setIsSplashScreenOpen(false);
-        sessionStorage.setItem("splash", "true");
-        setMessageSnackBarState({
-          message: t(snackBarStatusMessage["default"]),
-          isMessageSnackBarOpen: true,
-        });
-      }, 2000);
-    }
-  }, []);
 
   return (
     <main>
-      {isSplashScreenOpen && <SplashScreen />}
+      <SplashScreen />
       <TitleAppBar />
       {isPageName("qrcode") && <QrScannerPage />}
       {isPageName("cart") && (
