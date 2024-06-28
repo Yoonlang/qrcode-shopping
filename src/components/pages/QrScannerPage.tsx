b8bc7a40fcaf50b3be25d55feb42a48490ab142b
yoonlang <cdt9473@gmail.com>
2024-06-28 21:42:22 +0900

    feat: ErrorBoundary 구현

--- src/components/pages/QrScannerPage.tsx
+++ src/components/pages/QrScannerPage.tsx
@@ -1,18 +1,49 @@
+import { Button, CircularProgress } from "@mui/material";
 import { Suspense, useEffect, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { useTranslation } from "react-i18next";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
 
 import { snackBarStatusMessage } from "@/components/const";
 import MessageDialog from "@/components/MessageDialog";
 import QrCode from "@/components/QrScanner/QrCode";
 import useScannedItemList from "@/hooks/useScannedItemList";
+import { fetchedItemListCounter } from "@/recoil/atoms/fetchedItemListState";
 import { messageSnackBarState } from "@/recoil/atoms/messageSnackBarState";
 
 const StyledContainer = styled.div`
+  display: flex;
   width: 100%;
   height: 100%;
+  justify-content: center;
+  align-items: center;
 `;
+
+const RetryButton = ({ resetErrorBoundary }) => {
+  const setCounter = useSetRecoilState(fetchedItemListCounter);
+
+  const updateFetchedItemList = () => {
+    setCounter((old) => old + 1);
+  };
+
+  return (
+    <Button
+      color="error"
+      variant="contained"
+      onClick={() => {
+        updateFetchedItemList();
+        resetErrorBoundary();
+      }}
+    >
+      retry
+    </Button>
+  );
+};
+
+const fallbackRender = ({ resetErrorBoundary }) => {
+  return <RetryButton resetErrorBoundary={resetErrorBoundary} />;
+};
 
 const QrScannerPage = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(true);
@@ -46,9 +77,11 @@ const QrScannerPage = () => {
         messageList={[t("Dialog1"), t("Dialog2")]}
       />
       {!isDialogOpen && (
-        <Suspense fallback={<></>}>
-          <QrCode />
-        </Suspense>
+        <ErrorBoundary fallbackRender={fallbackRender}>
+          <Suspense fallback={<CircularProgress />}>
+            <QrCode />
+          </Suspense>
+        </ErrorBoundary>
       )}
     </StyledContainer>
   );
