5d4484c4b5f1d954a5c6323fcdbbd36040a9323d
yoonlang <cdt9473@gmail.com>
2024-07-28 16:00:21 +0900

    feat: 폴더 생성 모달 로직 구현

--- src/components/Manager/Folder/FolderCreationModal.tsx
+++ src/components/Manager/Folder/FolderCreationModal.tsx
@@ -1,5 +1,9 @@
-import { Button, Modal } from "@mui/material";
+import { Button, Modal, TextField } from "@mui/material";
+import { Field, Form, Formik } from "formik";
 import { styled } from "styled-components";
+import * as Yup from "yup";
+
+import { postFolder } from "@/api";
 
 const StyledModalContainer = styled.div`
   position: absolute;
@@ -11,12 +15,52 @@ const StyledModalContainer = styled.div`
   background-color: var(--color-white);
 `;
 
+const creationValidationSchema = Yup.object({
+  name: Yup.string().required(),
+  type: Yup.string().oneOf(["user", "product"]).required(),
+});
+
 const FolderCreationModal = ({ isModalOpen, onClose, type }) => {
   return (
     <Modal open={isModalOpen} onClose={onClose}>
       <StyledModalContainer>
         {type} 폴더 생성
-        <Button onClick={onClose}>닫기</Button>
+        <Formik
+          initialValues={{
+            name: "",
+            type,
+          }}
+          validationSchema={creationValidationSchema}
+          onSubmit={async (values, { setSubmitting }) => {
+            await postFolder(
+              JSON.stringify(values),
+              () => {
+                setSubmitting(false);
+                onClose();
+              },
+              (e) => {
+                console.log(e);
+              }
+            );
+          }}
+        >
+          {({ isSubmitting, errors, touched }) => (
+            <Form>
+              <Field
+                as={TextField}
+                name="name"
+                label="폴더 이름"
+                fullWidth
+                error={touched.name && errors.name}
+                helperText={touched.name && errors.name}
+              />
+              <Button type="submit" disabled={isSubmitting}>
+                생성
+              </Button>
+              <Button onClick={onClose}>닫기</Button>
+            </Form>
+          )}
+        </Formik>
       </StyledModalContainer>
     </Modal>
   );
