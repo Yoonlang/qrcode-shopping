b2479a7e0f7f6716a51fb616239fe0918bee03ea
yoonlang <cdt9473@gmail.com>
2024-08-04 17:31:51 +0900

    feat: 엑셀 제품 데이터 분류 함수 구현

--- src/components/Manager/Product/util.ts
+++ src/components/Manager/Product/util.ts
@@ -1,4 +1,9 @@
-import { ProductTableRow } from "@/components/Manager/Product/const";
+import {
+  ErrorProductExcel,
+  ProductExcel,
+  productExcelSchema,
+  ProductTableRow,
+} from "@/components/Manager/Product/const";
 import { Folder, Product } from "@/const";
 
 export const handleProductListForTable = (
@@ -15,4 +20,38 @@ export const handleProductListForTable = (
       __product__: product,
     };
   });
+};
+
+export const handleExcelFileProductList = async (
+  productExcelList: ProductExcel[],
+  productList: Product[]
+): Promise<{
+  newProductList: ProductExcel[];
+  existingProductList: ProductExcel[];
+  errorProductList: ErrorProductExcel[];
+}> => {
+  const processProductList = async () => {
+    const newProductList: ProductExcel[] = [];
+    const existingProductList: ProductExcel[] = [];
+    const errorProductList: ErrorProductExcel[] = [];
+    for (const pe of productExcelList) {
+      try {
+        await productExcelSchema.validate(pe);
+        if (productList.find((p) => p.productId === pe["제품"])) {
+          existingProductList.push(pe);
+        } else {
+          newProductList.push(pe);
+        }
+      } catch (e) {
+        errorProductList.push(pe);
+      }
+    }
+    return {
+      newProductList,
+      existingProductList,
+      errorProductList,
+    };
+  };
+
+  return processProductList();
 };
