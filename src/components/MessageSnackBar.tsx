17d12b2c479281507df6da07cad41ec4f3b93c62
yoonlang <cdt9473@gmail.com>
2024-08-06 13:59:06 +0900

    feat: MessageSnackBar 스캔 완료 시 스타일

--- src/components/MessageSnackBar.tsx
+++ src/components/MessageSnackBar.tsx
@@ -2,16 +2,32 @@ import styled from "@emotion/styled";
 import { Snackbar } from "@mui/material";
 import { useRecoilState } from "recoil";
 
+import { snackBarStatusMessage } from "@/components/const";
 import { messageSnackBarState } from "@/recoil/atoms/messageSnackBarState";
+import { useTranslation } from "react-i18next";
 
-const StyledSnackBar = styled(Snackbar)`
+interface StyledSnackBarProps {
+  isScanned: boolean;
+}
+
+const StyledSnackBar = styled(Snackbar, {
+  shouldForwardProp: (prop) => !["isScanned"].includes(prop),
+})<StyledSnackBarProps>`
   display: flex;
   justify-content: center;
   z-index: 1201;
 
   .MuiSnackbarContent-root {
-    width: 320px;
-    min-height: 80px;
+    ${(props) =>
+      props.isScanned
+        ? `
+        width: 250px;
+        height: 250px;
+        `
+        : `
+        width: 320px;
+        min-height: 80px;
+        `}
     position: fixed;
     top: 50%;
     left: 50%;
@@ -26,9 +42,11 @@ const StyledSnackBar = styled(Snackbar)`
 const MessageSnackBar = () => {
   const [{ message, isMessageSnackBarOpen }, setMessageSnackBarState] =
     useRecoilState(messageSnackBarState);
+  const { t } = useTranslation();
 
   return (
     <StyledSnackBar
+      isScanned={message === t(snackBarStatusMessage["scanned"])}
       open={isMessageSnackBarOpen}
       autoHideDuration={800}
       onClose={() => {
