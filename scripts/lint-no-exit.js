079d3b966bd6844bc6d2bdd4cd453d5f9c748031
yoonlang <cdt9473@gmail.com>
2024-08-07 01:15:16 +0900

    chore: commit 속도 향상을 위한 fix 생략 처리

--- scripts/lint-no-exit.js
+++ scripts/lint-no-exit.js
@@ -1,7 +1,7 @@
 import { execSync } from "child_process";
 
 try {
-  execSync("npx eslint src --fix --cache", { stdio: "inherit" });
+  execSync("npx eslint src --cache", { stdio: "inherit" });
 } catch (error) {
   console.log(
     "전체 lint 검사에서 문제가 발견되었습니다. 이후 스테이징 된 코드를 검사합니다."
