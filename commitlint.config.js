95d9b9ac30f1df3153d33d5d26c02d8c8a40e355
yoonlang <cdt9473@gmail.com>
2024-08-12 17:06:21 +0900

    chore: commitlint config 설정

---
+++ commitlint.config.js
@@ -0,0 +1,39 @@
+const OFF = 0;
+const WARNING = 1;
+const ERROR = 2;
+
+const typeList = [
+  "feat",
+  "fix",
+  "docs",
+  "style",
+  "refactor",
+  "test",
+  "chore",
+  "asset",
+];
+
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [ERROR, "always", 72],
+    "type-enum": [ERROR, "always", typeList],
+    "type-case": [ERROR, "always", "lower-case"],
+  },
+  plugins: [
+    {
+      rules: {
+        "header-prefix-type-custom-rule": ({ header }) => {
+          const regex = new RegExp(`^(${typeList.join("|")}): .+$`);
+          const isValid = regex.test(header);
+          return [
+            isValid,
+            '커밋 메시지는 "{type}: ~~" 형식을 따라야 합니다.\n가능한 타입: ' +
+              typeList.join(", "),
+            isValid ? OFF : ERROR,
+          ];
+        },
+      },
+    },
+  ],
+};
