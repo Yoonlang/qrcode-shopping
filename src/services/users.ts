06701f81e2ae4f7bc8cbf6850fe0d953c86915e0
yoonlang <cdt9473@gmail.com>
2024-09-04 13:24:59 +0900

    fix: orderer에서 user로 변수명 변경

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,8 +1,8 @@
 import { deleteUser } from "@/api/users";
 
-export const deleteUserList = (ordererList: string[]) => {
-  const deletePromises = ordererList.map((ordererId) => {
-    return deleteUser(undefined, ordererId);
+export const deleteUserList = (userList: string[]) => {
+  const deletePromises = userList.map((userId) => {
+    return deleteUser(undefined, userId);
   });
 
   return Promise.all(deletePromises);
