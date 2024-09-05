b022797cff3fc812a1abfc6c3cf452e06e2f06f0
yoonlang <cdt9473@gmail.com>
2024-09-05 12:04:10 +0900

    test: weight 고정

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -12,14 +12,17 @@ export function generateStaticParams() {
 
 const NotoSans = Noto_Sans({
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 const NotoSansSc = Noto_Sans_SC({
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 const NotoSansJp = Noto_Sans_JP({
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 const { title, description, url, ogImage, siteName, type } = META;
