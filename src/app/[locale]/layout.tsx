2361494017ab8eed0cdf75d7e459fc6893975175
yoonlang <cdt9473@gmail.com>
2024-09-05 14:38:28 +0900

    feat: 폰트 처리를 page에서 담당하도록 개선

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,26 +1,14 @@
 import { dir } from "i18next";
 import { Metadata } from "next";
-import { Noto_Sans, Noto_Sans_JP, Noto_Sans_SC } from "next/font/google";
 import { ReactNode } from "react";
 
 import { META } from "@/components/const";
+import { Language } from "@/const";
 import { i18nConfig } from "@/i18n";
 
 export function generateStaticParams() {
   return i18nConfig.locales.map((locale) => ({ locale }));
 }
-
-const NotoSans = Noto_Sans({
-  subsets: ["latin"],
-});
-
-const NotoSansSc = Noto_Sans_SC({
-  subsets: ["latin"],
-});
-
-const NotoSansJp = Noto_Sans_JP({
-  subsets: ["latin"],
-});
 
 const { title, description, url, ogImage, siteName, type } = META;
 
@@ -55,29 +43,18 @@ export const metadata: Metadata = {
   },
 };
 
-const getFontByLocale = (locale) => {
-  switch (locale) {
-    case "zh":
-      return NotoSansSc.className;
-    case "ja":
-      return NotoSansJp.className;
-    default:
-      return NotoSans.className;
-  }
-};
-
 export default function RootLayout({
   children,
   params: { locale },
 }: {
   children: ReactNode;
   params: {
-    locale: string;
+    locale: Language;
   };
 }) {
   return (
     <html lang={locale} dir={dir(locale)}>
-      <body className={getFontByLocale(locale)}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
