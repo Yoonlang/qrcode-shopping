b1df68bad1f7e424d9033cd285216568fcace735
yoonlang <cdt9473@gmail.com>
2026-03-19 15:04:43 +0900

    [AI] feat: Change AI agent channel color from sky blue to blue

--- src/globalStyles.tsx
+++ src/globalStyles.tsx
@@ -9,7 +9,7 @@ const GlobalStyle = () => (
         --color-white: #ffffff;
         --color-red: #ff0000;
         --color-green: #42ff00;
-        --color-blue: #3da3f5;
+        --color-blue: #0000FF; /* Changed from #3da3f5 to #0000FF */
         --color-gray-10: #fafafa;
         --color-gray-20: #f5f5f5;
         --color-gray-30: #eeeeee;
